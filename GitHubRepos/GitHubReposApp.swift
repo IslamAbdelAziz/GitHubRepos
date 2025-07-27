9597a604af7b798d8e459f7c441c9942a97ecd93
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2025-07-27 17:13:42 +0300

    Initial Commit

---
+++ GitHubRepos/GitHubReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubReposApp.swift
+//  GitHubRepos
+//
+//  Created by iSlam AbdelAziz on 27/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitHubReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

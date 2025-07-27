45c4698b5faca9cd6438fa4e794d8751ee5e6d4d
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2025-07-28 00:42:59 +0300

    [+] fix: App icon resize

---
+++ GitHubRepos/Utilities/Date+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Date+Extensions.swift
+//  GitHubRepos
+//
+//  Created by iSlam AbdelAziz on 27/07/2025.
+//
+
+import Foundation
---
+++ GitHubRepos/Views/RepoListSubViews/DateView.swift
@@ -0,0 +1,18 @@
+//
+//  DateView.swift
+//  GitHubRepos
+//
+//  Created by iSlam AbdelAziz on 27/07/2025.
+//
+
+import SwiftUI
+
+struct DateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateView()
+}

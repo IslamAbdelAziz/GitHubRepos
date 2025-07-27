a375dde9f67ddc0a20d2381310a83333f1399b8f
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2025-07-28 02:07:14 +0300

    [+] feat: Create Empty State

--- GitHubRepos/Views/ReposListView.swift
+++ GitHubRepos/Views/ReposListView.swift
@@ -12,45 +12,63 @@ struct ReposListView: View {
     @Namespace var namespace
     
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(repoDataStore.reposList) { repo in
-                    NavigationLink {
-                        RepoDetailsView(repo: repo)
-                            .navigationTransition(.zoom(sourceID: repo.id, in: namespace))
-                    } label: {
-                        VStack(alignment: .leading) {
-                            // Repo name
-                            Text(repo.name ?? "")
-                                .font(.largeTitle)
-                                .fontWeight(.bold)
-                                .foregroundStyle(.black.opacity(0.8))
-                            
-                            HStack {
-                                Image(systemName: "calendar")
-                                    .foregroundStyle(.gray)
-                                DateView(date: repo.creationDate)
-                            }
-                            Spacer()
-                            HStack {
-                                // Owner Image
-                                AvatarView(imgUrl: repo.owner?.avatarUrl)
-                                // Owner Name
-                                Text(repo.owner?.login ?? "")
-                                    .font(.title2)
-                                    .multilineTextAlignment(.center)
-                            }
-                            .padding(.bottom)
-                        }
+        if repoDataStore.reposList.isEmpty {
+            ContentUnavailableView {
+                Image(.emptyFolder)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200)
+                    .foregroundStyle(.gray)
+            } description: {
+                Text("There are no repos yet, Fetch it please")
+                    .font(.headline)
+            } actions: {
+                Button ("Fetch Repos", systemImage: "antenna.radiowaves.left.and.right") {
+                    Task {
+                        try await repoDataStore.loadRepos()
                     }
-                    .matchedTransitionSource(id: repo.id, in: namespace)
                 }
+                .buttonStyle(.borderedProminent)
+                .font(.title3)
+
             }
-            .navigationTitle(Text("GitHub Repos"))
-        }
-        .onAppear {
-            Task {
-                try await repoDataStore.loadRepos()
+
+        } else {
+            NavigationStack {
+                List {
+                    ForEach(repoDataStore.reposList) { repo in
+                        NavigationLink {
+                            RepoDetailsView(repo: repo)
+                                .navigationTransition(.zoom(sourceID: repo.id, in: namespace))
+                        } label: {
+                            VStack(alignment: .leading) {
+                                // Repo name
+                                Text(repo.name ?? "")
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                    .foregroundStyle(.black.opacity(0.8))
+                                
+                                HStack {
+                                    Image(systemName: "calendar")
+                                        .foregroundStyle(.gray)
+                                    DateView(date: repo.creationDate)
+                                }
+                                Spacer()
+                                HStack {
+                                    // Owner Image
+                                    AvatarView(imgUrl: repo.owner?.avatarUrl)
+                                    // Owner Name
+                                    Text(repo.owner?.login ?? "")
+                                        .font(.title2)
+                                        .multilineTextAlignment(.center)
+                                }
+                                .padding(.bottom)
+                            }
+                        }
+                        .matchedTransitionSource(id: repo.id, in: namespace)
+                    }
+                }
+                .navigationTitle(Text("GitHub Repos"))
             }
         }
     }
